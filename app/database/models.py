7b40ce9fb2e509b0a3f8dc2814c97522da169902
Andrey C <a.chervonii@visionlabs.ru>
2022-01-24 01:20:41 +0300

    new

---
+++ app/database/models.py
@@ -0,0 +1,22 @@
+from geoalchemy2.types import Geometry
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Store(Base):
+    __tablename__ = 'stores'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    address = Column(String)
+    coordinates = Column(Geometry(geometry_type='POINT', srid=27700))
+
+
+class Like(Base):
+    __tablename__ = 'likes'
+    id = Column(Integer, primary_key=True)
+    store_id = Column(Integer, ForeignKey('stores.id'), primary_key=True)
+    positive = Column(Boolean)
+    user_from = Column(String)
