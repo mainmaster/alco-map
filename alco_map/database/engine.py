9099cade34951709d7229e7bce2b3bdf2862c7bb
Andrey C <a.chervonii@visionlabs.ru>
2022-01-24 22:09:57 +0300

    edit ci

--- alco_map/database/engine.py
+++ alco_map/database/engine.py
@@ -3,10 +3,10 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("ALCO_MAP_DB_HOST")
+DB_USER = os.getenv("ALCO_MAP_DB_USER")
+DB_PASS = os.getenv("ALCO_MAP_DB_PASSWORD")
+DB_NAME = os.getenv("ALCO_MAP_DB_DATABASE")
 
 engine = create_async_engine(
     f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}/{DB_NAME}", echo=True,
